4948f0acb57fef51b2728160ef800b5defdd1cc8
anchikov_aa <anchikov_aa@da.gov.ru>
2026-06-13 15:27:25 +0300

    первые два тест

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+public class FirstTest {
+    @Test
+    public void firstTest() {
+        System.out.println("Hello, Maven!");
+    }
+    @Test
+    public void secondTest() {
+        System.out.println("Hello, Maven!");
+    }
+
+}
