5c0580242c262686a1ceaa8db2517460ce0031d6
anchikov_aa <anchikov_aa@da.gov.ru>
2026-06-14 01:19:41 +0300

    все обновил

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,15 +1,21 @@
-import org.testng.annotations.Test;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.text;
+
 public class FirstTest {
-    @Test
-    public void firstTest() {
-        System.out.println("Hello, Maven!");
+
+    @BeforeEach // В JUnit 5 это аналог @BeforeMethod из TestNG
+    public void setUp() {
+        Configuration.timeout = 10000;
+        // Здесь будет настройка Selenide
     }
+
     @Test
-    public void secondTest() {
-        System.out.println("Hello, Maven!");
-    }
-    @Test
-    public void ThirdTest() {
-        System.out.println("Hello, Maven!");
+    public void userCanOpenDemoQa() {
+        open("https://demoqa.com");
+        //$(".main-header").shouldHave(text("Elements"));
+        // ... остальные проверки
     }
 }
